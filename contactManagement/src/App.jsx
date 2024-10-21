b07252f359f8d517c61f8bef23fffc122a51e0f9
Mina Nazari <mina.nazary66@gmail.com>
2024-10-21 17:24:12 +0330

    connectin to JSON SERVER by axios and showing contacts

--- contactManagement/src/App.jsx
+++ contactManagement/src/App.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from 'react'
-import { Route, Routes ,Navigate} from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Route, Routes, Navigate } from "react-router-dom"
 import { Navbar, Contact, Contacts, AddContact, EditContact, ViewContact } from './components'
 
 const App = () => {
   const [loading, setLoading] = useState(false)
-  const[getContacts, setContacts] = useState([])
+  const [getContacts, setContacts] = useState([])
+  const [getGroups, setGroups] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const {data : contactsData} = await axios.get("http://localhost:9000/contacts")
+        const {data : groupsData} = await axios.get("http://localhost:9000/groups")
+        setContacts(contactsData)
+        setGroups(groupsData)
+        setLoading(false)
+      } catch (err) {
+        console.log(err.message)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className='app'>
--- contactManagement/src/components/contacts/Contact.jsx
+++ contactManagement/src/components/contacts/Contact.jsx
@@ -3,27 +3,26 @@ import { CURRENTLINE, CYAN, ORANGE, RED } from "../../helpers/colors";
 import { FaRegEye } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 import { MdDelete } from "react-icons/md";
-import img1 from "../../../public/images/1.png"
 
-const Contact = () => {
+const Contact = ({contact}) => {
   return (
     <div className='col-md-6'>
       <div className="card my-2" style={{ backgroundColor: CURRENTLINE }}>
         <div className="card-body">
           <div className="row justify-content-around align-items-center">
             <div className="col-sm-4">
-              <img src={img1} alt="" className='rounded-2' />
+              <img src={contact.photo} alt={contact.fullname} className='rounded-2' />
             </div>
             <div className="col-sm-7">
               <ul className='list-group pe-2'>
                 <li className='list-group-item list-group-item-dark'>
-                  نام و نام خانوادگی: {" "} <span className='fw-bold'>مینا نظری</span>
+                  نام و نام خانوادگی: {" "} <span className='fw-bold'>{contact.fullname}</span>
                 </li>
                 <li className='list-group-item list-group-item-dark'>
-                  شماره تماس: {" "} <span className='fw-bold'>۰۹۱۲۴۵۴۸۳۶۱</span>
+                  شماره تماس: {" "} <span className='fw-bold'>{contact.mobile}</span>
                 </li>
                 <li className='list-group-item list-group-item-dark'>
-                  آدرس ایمیل: {" "} <span className='fw-bold'>mina.nazary66@gmail.com</span>
+                  آدرس ایمیل: {" "} <span className='fw-bold'>{contact.email}</span>
                 </li>
               </ul>
             </div>
